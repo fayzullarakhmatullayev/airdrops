696ea4c6a8fc5812e4983a497ca8481804f29cdc
Fayzullo <fayzullarakhmatullayev@gmail.com>
2024-10-15 17:35:33 +0500

    updated

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,1 +1,18 @@
-console.log('Scripts is working!');
+const svg = document.getElementById('mySVG');
+
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      svg.style.opacity = '1';
+      svg.style.clipPath = 'inset(0 0 0 0)';
+    }
+  });
+}, options);
+
+observer.observe(document.querySelector('.faq__block'));
